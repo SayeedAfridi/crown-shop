2f9cbab03dbdd18f342d26bfd0d2553e1867aecd
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-15 01:44:32 +0600

    Updates

---
+++ precache-manifest.46035890b7dd846f376ebe4d3505ddce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3758716040bb8a61dfb5755f5cfc8e",
+    "url": "/crown-shop/index.html"
+  },
+  {
+    "revision": "760632439a67323c73ed",
+    "url": "/crown-shop/static/css/main.343832e0.chunk.css"
+  },
+  {
+    "revision": "5be81dddd87a8c8a842d",
+    "url": "/crown-shop/static/js/2.506644f2.chunk.js"
+  },
+  {
+    "revision": "760632439a67323c73ed",
+    "url": "/crown-shop/static/js/main.67e9814c.chunk.js"
+  },
+  {
+    "revision": "9e23704876beb2404cd9",
+    "url": "/crown-shop/static/js/runtime~main.7fd36115.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-shop/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crown-shop/static/media/shopping-bag.30f07071.svg"
+  }
+]);
