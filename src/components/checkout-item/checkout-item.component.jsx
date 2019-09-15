b3e3ba288429809c19f944c309c0bac3ec152d12
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-16 00:05:16 +0600

    added increase or dicrease functionality to checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import './checkout-item.style.scss'
-import {clearItem} from '../../redux/cart/cart.actions'
+import {clearItem, addItem, removeItem} from '../../redux/cart/cart.actions'
 
-const CheckoutItem = ({cartItem, clearItem}) => {
+const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
     const {name, imageUrl, price, quantity} = cartItem
     return (
         <div className='checkout-item'>
@@ -11,8 +11,12 @@ const CheckoutItem = ({cartItem, clearItem}) => {
                 <img alt='item' src={imageUrl}/>
             </div>
             <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-            <span className='quantity'>{quantity}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={()=> removeItem(cartItem)}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={()=> addItem(cartItem)}>&#10095;</div>
+            </span>
+            <span className='price'>${price}</span>
             <div className='remove-button' onClick={() => clearItem(cartItem)}>
                 &#10005;
             </div>
@@ -22,7 +26,9 @@ const CheckoutItem = ({cartItem, clearItem}) => {
 }
 
 const mapDispatchtoProps = dispatch =>({
-    clearItem: (item) => dispatch(clearItem(item))
+    clearItem: (item) => dispatch(clearItem(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
 })
 
 
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,18 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }
+
+export const removeItemFromCart = (cartItems, toRemove) => {
+    const exists = cartItems.find(item => item.id === toRemove.id)
+
+    if(exists.quantity === 1){
+        return cartItems.filter(item => item.id !== toRemove.id)
+    }
+
+    return cartItems.map(item =>
+            item.id === toRemove.id ?
+                {...item, quantity: item.quantity-1}
+            :
+                item
+        )
+}
