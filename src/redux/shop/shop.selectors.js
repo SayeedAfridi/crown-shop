b0a2a1e1e73f52c0e30fc2e71156dbd761d90c06
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-22 01:06:45 +0600

    normalize shop datajs and use it in shop selector

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,5 @@
 import {createSelector} from 'reselect'
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
 
 const selectshop = state => state.shop
 
@@ -17,5 +10,5 @@ export const selectShopCollections = createSelector(
 
 export const selectCollection = urlParam => createSelector(   
     [selectShopCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[urlParam]) 
+    collections => collections[urlParam]
 )
