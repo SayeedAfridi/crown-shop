40ab50d04a4c2d6d7c3cb2ee38942482ce5b91d3
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-24 18:19:07 +0600

    css in js. Added styled Component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link } from 'react-router-dom'
-import './header.style.scss'
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles'
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component'
@@ -12,34 +11,34 @@ import {selectCartHidden} from '../../redux/cart/cart.selectors'
 
 const Header = ({currentUser, hidden}) => {
     return (
-        <div className='header'>
-            <Link to='/crown-shop' className='logo-container'>
+        <HeaderContainer>
+            <LogoContainer to='/crown-shop'>
                 <Logo className='logo'/>
-            </Link>
-            <div className='options'>
-                <Link to='/crown-shop/shop' className='option'>
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to='/crown-shop/shop'>
                     SHOP
-                </Link>
-                <Link to='/crown-shop/shop' className='option'>
+                </OptionLink>
+                <OptionLink to='/crown-shop/shop'>
                     CONTACT
-                </Link>
+                </OptionLink>
                 {
                     currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}>
+                    <OptionLink as='div' onClick={() => auth.signOut()}>
                         SIGN OUT
-                    </div>
+                    </OptionLink>
                     :
-                    <Link to='/crown-shop/signin' className='option'>
+                    <OptionLink to='/crown-shop/signin'>
                         SIGN IN
-                    </Link>
+                    </OptionLink>
                 }
                 <CartIcon />
                 {
                     hidden ? null : (<Cart />)
                 }
                 
-            </div>
-        </div>
+            </OptionsContainer>
+        </HeaderContainer>
     )
 }
 
--- src/pages/Hompage/homepage.component.jsx
+++ src/pages/Hompage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './homepage.style.scss'
+import {HomepageContainer} from './homepage.styles'
 import Directory from '../../components/directory/directory.component'
 
 const Homepage = () => {
     return(
-        <div className='homepage'>
+        <HomepageContainer>
             <Directory />
-        </div>
+        </HomepageContainer>
     )
 }
 
